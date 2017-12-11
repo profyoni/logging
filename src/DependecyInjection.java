e6830e33e124cda51010a4ca19d59dd548f41752
Jonathan Robinson <yrobinson@gmail.com>
2017-12-10 22:16:01 -0500

    first commit

--- src/DependecyInjection.java
+++ src/DependecyInjection.java
@@ -2,7 +2,7 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 public class DependecyInjection {
-
+//https://github.com/google/guice/wiki/Motivation
    // Injector injector = Guice.createInjector(new BillingModule());
    // BillingService billingService = injector.getInstance(BillingService.class);
 }
