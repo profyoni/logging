b5e34ba9109ffb5e3ee786733e7427696476830f
profyoni <jrobinson@touro.edu>
2017-12-11 15:48:11 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import com.google.inject.Injector;
 import org.apache.log4j.Logger;
 
 public class Main {
+    // https://logging.apache.org/log4j/2.x/manual/api.html
     final static Logger logger = Logger.getLogger(Main.class);
     public static void main(String[] args) {
         logger.debug("test");
